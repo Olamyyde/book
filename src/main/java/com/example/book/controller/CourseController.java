b187c2e77ca77219da42dc70fd4298d0b03a6348
olamide <myyde8170@gmail.com>
2022-11-11 21:38:58 +0100

    added controller method descriptions and status codes

--- src/main/java/com/example/book/controller/CourseController.java
+++ src/main/java/com/example/book/controller/CourseController.java
@@ -3,13 +3,17 @@ package com.example.book.controller;
 
 import com.example.book.model.Course;
 import com.example.book.service.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/courses/")
+@Tag(name = "Course Controller", description = "This Rest controller provides servivces to manage courses in the Course Tracker application")
 public class CourseController {
     private CourseService courseService;
 
@@ -19,36 +23,49 @@ public class CourseController {
     }
 
     @GetMapping
+    @Operation(summary = "Provides all courses available in the Course Tracker application")
+    @ResponseStatus(code = HttpStatus.OK)
     public Iterable<Course> getAllCourses() {
         return courseService.getCourses();
     }
 
     @GetMapping("{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides course details for the supplied course id from the Course Tracker application")
     public Optional<Course> getCourseById(@PathVariable("id") long courseId) {
         return courseService.getCourseById(courseId);
     }
 
     @GetMapping("category/{name}")
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides course details for the supplied course category from the Course Tracker application")
     public Iterable<Course> getCourseByCategory(@PathVariable("name") String category) {
         return courseService.getCoursesByCategory(category);
     }
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @Operation(summary = "Creates a new course in the Course Tracker application")
     public Course createCourse(@RequestBody Course course) {
         return courseService.createCourse(course);
     }
 
     @PutMapping("{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void updateCourse(@PathVariable("id") long courseId, @RequestBody Course course) {
         courseService.updateCourse(courseId, course);
     }
 
     @DeleteMapping("{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deletes the course details for the supplied course id from the Course Tracker application")
     void deleteCourseById(@PathVariable("id") long courseId) {
         courseService.deleteCourseById(courseId);
     }
 
     @DeleteMapping
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deletes all courses from the Course Tracker application")
     void deleteCourses() {
         courseService.deleteCourses();
     }
