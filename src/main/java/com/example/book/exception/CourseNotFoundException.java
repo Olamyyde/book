81053d54b588fb1f2b21cb99999f8f4926dcae10
olamide <myyde8170@gmail.com>
2022-11-13 03:57:55 +0100

    added serialVersionUID

--- src/main/java/com/example/book/exception/CourseNotFoundException.java
+++ src/main/java/com/example/book/exception/CourseNotFoundException.java
@@ -2,6 +2,7 @@ package com.example.book.exception;
 
 
 public class CourseNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 5071646428281007896L;
 
     public CourseNotFoundException(String message) {
         super(message);
